45f4a834b0be5e9de69025c7ec089a5df01fdb82
Andrew Malone <mandrew434@gmail.com>
2025-01-28 12:48:44 -0700

    Stuff

--- Program.cs
+++ Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-Console.WriteLine("This is code");
-
-Console.WriteLine("This is going to be a mistake");
